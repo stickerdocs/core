60cf97a8f68681f16671fbafe508e3a30d99b392
Anthony Fielding <anthony@stickerdocs.com>
2024-02-08 19:56:06 +0000

    Add osv-scanner as part of testing

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,11 +3,19 @@
 # Exit if any command fails
 set -e
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+CYAN='\033[1;36m'
+NC='\033[0m' # No Color
+
 # Generate mock objects
 dart run build_runner build --delete-conflicting-outputs
 
 # Run the Dart linter
 dart analyze
+
+echo -e "${CYAN}Running osv-scanner...${NC}"
+osv-scanner -lockfile=pubspec.lock
 
 # Run tests
 flutter test -r expanded \
