39fa20a48aa06cec6c5f72d4105589e62bea3912
Anthony Fielding <anthony@stickerdocs.com>
2024-08-22 19:42:38 +0100

    Update dependencies

--- scripts/upgrade_packages.sh
+++ scripts/upgrade_packages.sh
@@ -4,10 +4,7 @@
 set -e
 
 flutter pub upgrade
-
-# Disable this or it will pull down the broken version of libsodium
-# flutter pub upgrade --major-versions
-
+flutter pub upgrade --major-versions
 flutter clean
 flutter pub get
 flutter pub outdated
